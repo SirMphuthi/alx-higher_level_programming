44cb7fc160e8ac8f149c84cb8ca637adb11708c0
SirMphuthi <kgotsomphuthi@gmail.com>
2024-10-23 01:29:24 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Defines a function that divides a materix."""
+# 2-matrix_divided.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a matrix division function."""
 
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix.
 
-    Arguments:
-            matrix (list): A list of ;ists of ints or floats.
-            div (int/float): The divider.
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
     Raises:
         TypeError: If the matrix contains non-numbers.
         TypeError: If the matrix contains rows of different sizes.
         TypeError: If div is not an int or float.
         ZeroDivisionError: If div is 0.
-    Return:
-        A new matrix that returns a results of division.
+    Returns:
+        A new matrix representing the result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
@@ -22,6 +24,7 @@ def matrix_divided(matrix, div):
                     for ele in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
+
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
